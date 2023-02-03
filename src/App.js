fc7160cc5195ac9feda066dfed703903a7b529d4
mavu1707 <markus.yung.vullum@gmail.com>
2023-02-03 09:50:11 +0100

    Funker

---
+++ src/App.js
@@ -0,0 +1,13 @@
+/*import logo from './logo.svg';*/
+import './App.css';
+import './css/main.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hei</h1>
+    </div>
+  );
+}
+
+export default App;
