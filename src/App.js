7063f257804e13d9649cc31a1c715d2456e4ee7e
Vullum <markus.yung.vullum@gmail.com>
2023-02-04 15:13:32 +0100

    Mye ser likt ut

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
 import './css/main.css';
+
 import ProductCard from './components/ProductCard/ProductCard';
 import data from './components/ProductCard/products.json';
 
+import NavBar from './components/NavBar/NavBar';
+import nav from './components/NavBar/navlinks.json';
 
+import {ReactComponent as Logo} from './img/LD_logo.svg';
+import {ReactComponent as Cart} from './img/legocart.svg';
 //npm start
 //npm run css:watch
 //npm init
@@ -11,20 +16,35 @@ import data from './components/ProductCard/products.json';
 export default function App() {
   return (
     <div className="App">
-      <nav>
-        
-      </nav>
       <main>
-      {data.map((product) => (
-        <ProductCard
-          key={product.proid}
-          name={product.title}
-          price={product.price}
-          cat={product.category}
-        />
-      ))}
-    </main>
-  </div>
+        {/*Header*/}
+        <header>
+          <Logo className='logo'/>
+          <Cart className='cart'/>
+        </header>
+
+        {/*Nav Bar*/}
+        <nav>
+          {nav.map((navbar) => (
+            <NavBar
+              name={navbar.title}
+            />
+          ))}
+        </nav>
+          
+        <h2>Ninjago</h2>
+
+        {/*Product card*/}
+        {data.map((product) => (
+          <ProductCard
+            key={product.proid}
+            name={product.title}
+            price={product.price}
+            cat={product.category}
+          />
+        ))}
+      </main>
+    </div>
 
   )
 }
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,9 @@
+import NavbarName from "./NavbarName";
+
+export default function NavBar({name}){
+  return (
+    <a>
+      <NavbarName name={name} />
+    </a>
+  )
+}
